c2eade7567c3c6676dd88ab8e8506c9f4f5c1d60
Prz3mol <przemek190803@gmail.com>
2026-03-13 22:15:30 +0100

    Dodanie podstawowego kodu w C#

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -1,5 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Wypisywanie na konsolę
+Console.WriteLine("Witaj w projekcie APBD!");
+
+// Pobieranie danych od użytkownika
+Console.Write("Jak masz na imię? ");
+string user = Console.ReadLine();
+
+Console.WriteLine($"Cześć {user}, zaczynamy naukę C#!");
+
+// Prosta instrukcja warunkowa
+int punkty = 55;
+
+if (punkty >= 50) 
+{
+    Console.WriteLine("Zaliczone!");
+} 
+else 
+{
+    Console.WriteLine("Spróbuj jeszcze raz.");
+}
+
+// Pętla for - wykonaj coś 5 razy
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"To jest obieg nr: {i}");
+}
 
 Console.WriteLine("Press any key to exit...");
